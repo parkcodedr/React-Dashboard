d05c7438414ab416a8b0c7664d3e0cc4fc631bd3
parkcodedr <okwori32@gmail.com>
2022-05-23 13:05:13 +0100

    theme context

--- src/components/ChartsHead.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ChartsHead = () => {
-  return (
-    <div>ChartsHead</div>
-  )
-}
-
-export default ChartsHead
--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
 import './index.css';
+import {ContextProvider} from './contexts/ContextProvider';
 
-ReactDom.render(<App/>,document.getElementById('root'));
+ReactDom.render(
+    <ContextProvider>
+        <App/>
+    </ContextProvider>,document.getElementById('root'));
